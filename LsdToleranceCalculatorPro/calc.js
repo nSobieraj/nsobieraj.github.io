898daa2bc282d96712970c9df9f338b42a6ba26d
Nikodem <nsobieraj.a@tuta.io>
2021-10-11 12:06:05 +0200

    Update calc.js

--- LsdToleranceCalculatorPro/calc.js
+++ LsdToleranceCalculatorPro/calc.js
@@ -38,7 +38,7 @@ function calculate() {
             }
 
             // calcDose += ((280.059565 * (Math.pow(dose.days + i, -0.412565956))) * (parseFloat(dose.dose) / 100) - dose.dose);
-            calcDose += ((280.059565 * (Math.pow(i, -0.412565956))) * (parseFloat(dose.dose) / 100) - dose.dose);
+            calcDose += ((280.059565 * (Math.pow(dose.days + i, -0.412565956))) * (parseFloat(dose.dose) / 100) - dose.dose);
             if(calcDose <= desiredDose){
                 calcDose = 0;
             }
